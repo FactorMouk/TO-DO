3f72b62728cc734b0478e7df326e457d1f190f18
FactorMouk <jvoa@cin.ufpe.br>
2021-07-06 15:42:31 -0300

    Adding firebase connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAeAAAg4UUVH9NKMwwqXA2d1Uca1CbGXYo",
+  authDomain: "to-dos-app-f1b05.firebaseapp.com",
+  projectId: "to-dos-app-f1b05",
+  storageBucket: "to-dos-app-f1b05.appspot.com",
+  messagingSenderId: "641365083878",
+  appId: "1:641365083878:web:ef0c3ce5f1dd487a6b9d27",
+  measurementId: "G-BCC47MBB8W"
+});
 
 ReactDOM.render(
   <React.StrictMode>
