7a846cb52437799b9c7baaf1c4ba95279a1819b5
FactorMouk <jvoa@cin.ufpe.br>
2021-07-13 12:11:10 -0300

    Addind PWA configs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import ToDoList from "./components/to-do-list/ToDoList";
+import React, { Suspense, lazy } from "react";
 import "./App.scss";
 import toDoLogo from "./assets/imgs/to-do-logo.png";
 import backIllust1 from "./assets/imgs/background-illust-1.png";
 import backIllust2 from "./assets/imgs/background-illust-2.png";
 
+const ToDoList = lazy(() => import("./components/to-do-list/ToDoList"));
+
 function App() {
   return (
-    <div className="ToDoApp">
-      <header>
-        <img src={toDoLogo}></img>
-      </header>
-      <div className="main-container">
-        <div className="content">
-          <ToDoList />
+    <Suspense fallback={<div></div>}>
+      <main>
+        <div className="ToDoApp">
+          <header>
+            <img src={toDoLogo} alt="logo"></img>
+          </header>
+          <div className="main-container">
+            <div className="content">
+              <ToDoList />
+            </div>
+            <div className="background">
+              <img
+                className="back-illust1"
+                src={backIllust1}
+                alt="backIllust1"
+              ></img>
+              <img
+                className="back-illust2"
+                src={backIllust2}
+                alt="backIllust2"
+              ></img>
+            </div>
+          </div>
+          <footer>
+            <p>
+              Um teste projetado pela{" "}
+              <a href="https://guava.software/">Guava</a>
+            </p>
+            <p>
+              Ilustrações por
+              <a href="https://www.opendoodles.com/"> Open Doodles</a> | Inter
+              UI Font Family por
+              <a href="https://rsms.me/"> Rasmus Andersson</a>
+            </p>
+          </footer>
         </div>
-        <div className="background">
-          <img className="back-illust1" src={backIllust1}></img>
-          <img className="back-illust2" src={backIllust2}></img>
-        </div>
-      </div>
-      <footer>
-        <p>
-          Um teste projetado pela <a href="https://guava.software/">Guava</a>
-        </p>
-        <p>
-          Ilustrações por
-          <a href="https://www.opendoodles.com/"> Open Doodles</a> | Inter UI
-          Font Family por
-          <a href="https://rsms.me/"> Rasmus Andersson</a>
-        </p>
-      </footer>
-    </div>
+      </main>
+    </Suspense>
   );
 }
 
